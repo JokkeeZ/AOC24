72ab332797a445addc5a2f5c98c8411f0b4286ad
Joel <11576776+JokkeeZ@users.noreply.github.com>
2024-12-13 23:05:18 +0200

    Day 6 - Part 2 done (slow but works :D)

--- Day 6/Day6.cs
+++ Day 6/Day6.cs
@@ -2,19 +2,26 @@
 
 class Day6 : IAdventDay
 {
+	private readonly bool draw = false;
+
 	private char[,] map;
+	private (int x, int y) guard;
+	private Direction direction;
+	private (int x, int y) startCell;
+
+	private readonly HashSet<(int x, int y)> visitedCells = [];
+
 	private enum Direction
 	{
 		Up,
+		Right,
 		Down,
-		Left,
-		Right
+		Left
 	}
 
 	public void Solve(string[] input)
 	{
 		map = new char[input[0].Length, input.Length];
-		var position = (x: 0, y: 0);
 
 		for (var y = 0; y < input.Length; ++y)
 		{
@@ -22,8 +29,8 @@ class Day6 : IAdventDay
 			{
 				if (input[y][x] == '^')
 				{
-					position = (x, y);
-					map[x, y] = 'X';
+					guard = (x, y);
+					startCell = (x, y);
 				}
 				else
 				{
@@ -32,58 +39,136 @@ class Day6 : IAdventDay
 			}
 		}
 
-		var direction = Direction.Up;
+		var visitedCells = new HashSet<(int x, int y)>
+		{
+			(guard.x, guard.y)
+		};
 
 		while (true)
 		{
-			var (nextX, nextY) = direction switch
+			if (draw)
 			{
-				Direction.Up => (position.x, position.y - 1),
-				Direction.Down => (position.x, position.y + 1),
-				Direction.Left => (position.x - 1, position.y),
-				Direction.Right => (position.x + 1, position.y),
-				_ => throw new NotImplementedException(),
-			};
+				DrawMap();
+			}
 
-			if (nextX < 0 || nextY < 0 || nextX >= map.GetLength(0) || nextY >= map.GetLength(1))
+			var (nextX, nextY) = GetNextCell((guard.x, guard.y), direction);
+			if (!map.InBounds(nextX, nextY))
 			{
-				Console.WriteLine($"Part 1: {GetVisitedCellsCount()}");
 				break;
 			}
 
-			if (map[nextX, nextY] is not '.' and not 'X')
+			if (map[nextX, nextY] is '#')
 			{
-				direction = direction switch
-				{
-					Direction.Up => Direction.Right,
-					Direction.Down => Direction.Left,
-					Direction.Left => Direction.Up,
-					Direction.Right => Direction.Down,
-					_ => throw new NotImplementedException(),
-				};
-
+				direction = GetNextRotation(direction);
 				continue;
 			}
 
-			position = (nextX, nextY);
-			map[nextX, nextY] = 'X';
+			guard = (nextX, nextY);
+			visitedCells.Add((guard.x, guard.y));
+		}
+
+		Console.WriteLine($"Part 1: {visitedCells.Count}");
+
+		var count = 0;
+		foreach (var cell in visitedCells)
+		{
+			if (IsLoop(cell, startCell))
+			{
+				count++;
+			}
+		}
+
+		Console.WriteLine($"Part 2: {count}");
+	}
+
+	private bool IsLoop((int x, int y) cell, (int x, int y) current)
+	{
+		var currentDir = Direction.Up;
+
+		var loopPath = new HashSet<((int x, int y), Direction)>()
+		{
+			(current, currentDir)
+		};
+
+		while (true)
+		{
+			var newCell = GetNextCell((current.x, current.y), currentDir);
+
+			if (!map.InBounds(newCell.x, newCell.y))
+			{
+				return false;
+			}
+
+			if (map[newCell.x, newCell.y] == '#' || newCell == cell)
+			{
+				currentDir = GetNextRotation(currentDir);
+				continue;
+			}
+
+			current = newCell;
+
+			if (!loopPath.Add((current, currentDir)))
+			{
+				return true;
+			}
 		}
 	}
 
-	private int GetVisitedCellsCount()
+	private static Direction GetNextRotation(Direction dir)
 	{
-		var count = 0;
+		return dir switch
+		{
+			Direction.Up => Direction.Right,
+			Direction.Down => Direction.Left,
+			Direction.Left => Direction.Up,
+			Direction.Right => Direction.Down,
+			_ => throw new("HUH")
+		};
+	}
+
+	private static (int x, int y) GetNextCell((int x, int y) pos, Direction dir)
+	{
+		return dir switch
+		{
+			Direction.Up => (pos.x, pos.y - 1),
+			Direction.Right => (pos.x + 1, pos.y),
+			Direction.Down => (pos.x, pos.y + 1),
+			Direction.Left => (pos.x - 1, pos.y),
+			_ => throw new("HUH"),
+		};
+	}
+
+	private void DrawMap()
+	{
+		Console.Clear();
+
 		for (var y = 0; y < map.GetLength(1); ++y)
 		{
 			for (var x = 0; x < map.GetLength(0); ++x)
 			{
-				if (map[x, y] == 'X')
+				var visitedCell = visitedCells.Contains((x, y));
+
+				if (guard.x == x && guard.y == y && visitedCell)
 				{
-					count++;
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.Write('@');
+					Console.ResetColor();
+				}
+				else if (visitedCell)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.Write('X');
+					Console.ResetColor();
+				}
+				else
+				{
+					Console.Write(map[x, y]);
 				}
 			}
+
+			Console.WriteLine();
 		}
 
-		return count;
+		Thread.Sleep(TimeSpan.FromMilliseconds(50));
 	}
 }
---
+++ ExtensionMethods.cs
@@ -0,0 +1,7 @@
+﻿namespace AOC24;
+
+public static class ExtensionMethods
+{
+	public static bool InBounds<T>(this T[,] map, int x, int y) where T : IEquatable<T>
+		=> x >= 0 && y >= 0 && x < map.GetLength(0) && y < map.GetLength(1);
+}
