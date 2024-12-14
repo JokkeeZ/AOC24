264df5bb1de5dd35b4e77818b24087c1678040fd
Joel <11576776+JokkeeZ@users.noreply.github.com>
2024-12-14 05:44:22 +0200

    Day 7

---
+++ Day 7/Day7.cs
@@ -0,0 +1,97 @@
+﻿namespace AOC24;
+
+class Day7 : IAdventDay
+{
+	public bool IsActive => true;
+
+	public void Solve(string[] input)
+	{
+		var equations = new Dictionary<long, List<long>>();
+
+		foreach (var line in input)
+		{
+			var split = line.Split(": ");
+
+			equations.Add(long.Parse(split[0]),
+				split[1].Split(' ')
+				.Select(long.Parse)
+				.ToList());
+		}
+
+		long calibrationResult = 0;
+		foreach (var (sum, numbers) in equations)
+		{
+			if (IsValidEquation(numbers, sum))
+			{
+				calibrationResult += sum;
+			}
+		}
+
+		Console.WriteLine($"Part 1: {calibrationResult}");
+
+		calibrationResult = 0;
+		foreach (var (sum, numbers) in equations)
+		{
+			if (IsValidEquationWithConcat(numbers, sum))
+			{
+				calibrationResult += sum;
+			}
+		}
+
+		Console.WriteLine($"Part 2: {calibrationResult}");
+	}
+
+	static bool IsValidEquation(List<long> nums, long sum)
+	{
+		var stack = new Stack<(long current, int index)>();
+		stack.Push((nums[0], 1));
+
+		while (stack.Count > 0)
+		{
+			var (current, index) = stack.Pop();
+
+			if (index == nums.Count - 1)
+			{
+				if (current + nums[index] == sum || current * nums[index] == sum)
+				{
+					return true;
+				}
+				continue;
+			}
+
+			stack.Push((current + nums[index], index + 1));
+			stack.Push((current * nums[index], index + 1));
+		}
+
+		return false;
+	}
+
+	static bool IsValidEquationWithConcat(List<long> numbers, long sum)
+	{
+		var stack = new Stack<(long current, int index)>();
+		stack.Push((numbers[0], 1));
+
+		while (stack.Count > 0)
+		{
+			var (current, index) = stack.Pop();
+
+			if (index == numbers.Count - 1)
+			{
+				if (current + numbers[index] == sum ||
+					current * numbers[index] == sum ||
+					long.Parse($"{current}{numbers[index]}") == sum)
+				{
+					return true;
+				}
+
+				continue;
+			}
+
+			stack.Push((current + numbers[index], index + 1));
+			stack.Push((current * numbers[index], index + 1));
+			stack.Push((long.Parse($"{current}{numbers[index]}"), index + 1));
+		}
+
+		return false;
+	}
+}
