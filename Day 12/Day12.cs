d50c6ca8f2cf1fe318c3ef68b22c6a96a56773b7
Joel <11576776+JokkeeZ@users.noreply.github.com>
2024-12-22 01:07:50 +0200

    Day 12 - Part 2 (forgot to push yesterday)

--- Day 12/Day12.cs
+++ Day 12/Day12.cs
@@ -2,16 +2,14 @@
 
 class Day12 : IAdventDay
 {
-	public bool IsActive => true;
+	public bool IsActive => false;
 
 	private char[,] map;
-	private bool[,] visited;
 	private readonly List<GardenArea> areas = [];
 
 	public void Solve(string[] input)
 	{
 		map = new char[input[0].Length, input.Length];
-		visited = new bool[input[0].Length, input.Length];
 
 		for (var y = 0; y < input.Length; ++y)
 		{
@@ -21,6 +19,7 @@ class Day12 : IAdventDay
 			}
 		}
 
+		var visited = new bool[input[0].Length, input.Length];
 		for (var y = 0; y < map.GetLength(1); ++y)
 		{
 			for (var x = 0; x < map.GetLength(0); ++x)
@@ -38,7 +37,7 @@ class Day12 : IAdventDay
 				area.Plants.Add((x, y));
 				visited[x, y] = true;
 
-				MakeGardenArea(area, x, y);
+				MakeGardenArea(area, x, y, visited);
 
 				areas.Add(area);
 			}
@@ -53,20 +52,68 @@ class Day12 : IAdventDay
 			}
 		}
 
-
 		Console.WriteLine($"Part 1: {areas.Sum(a => a.Plants.Count * a.FenceCount)}");
+
+		foreach (var area in areas)
+		{
+			CountAreaSides(area);
+		}
+
+		Console.WriteLine($"Part 2: {areas.Sum(a => a.Plants.Count * a.Sides)}");
 	}
 
-	private void MakeGardenArea(GardenArea area, int x, int y)
+	static void CountAreaSides(GardenArea area)
+	{
+		foreach (var (x, y) in area.Plants)
+		{
+			var left = (x - 1, y);
+			var top = (x, y - 1);
+			var topleft = (x - 1, y - 1);
+			var right = (x + 1, y);
+			var topright = (x + 1, y - 1);
+			var bottom = (x, y + 1);
+			var bottomleft = (x - 1, y + 1);
+
+			if (CheckForEdge(area, top, left, topleft))
+			{
+				area.Sides++;
+			}
+
+			if (CheckForEdge(area, top, right, topright))
+			{
+				area.Sides++;
+			}
+
+			if (CheckForEdge(area, left, top, topleft))
+			{
+				area.Sides++;
+			}
+
+			if (CheckForEdge(area, left, bottom, bottomleft))
+			{
+				area.Sides++;
+			}
+		}
+	}
+
+	private void MakeGardenArea(GardenArea area, int x, int y, bool[,] visited)
 	{
 		foreach (var (X, Y) in map.GetNeighbors(x, y).Where(p => map[p.X, p.Y] == area.Name && !visited[p.X, p.Y]))
 		{
 			area.Plants.Add((X, Y));
 			visited[X, Y] = true;
 
-			MakeGardenArea(area, X, Y);
+			MakeGardenArea(area, X, Y, visited);
 		}
 	}
+
+	static bool CheckForEdge(GardenArea area, (int, int) dir1, (int, int) dir2, (int, int) dir3)
+	{
+		return !area.Plants.Contains(dir2)
+			&& (!area.Plants.Contains(dir1)
+			|| area.Plants.Contains(dir3));
+	}
+
 }
 
 class GardenArea
@@ -74,4 +121,5 @@ class GardenArea
 	public char Name { get; set; }
 	public HashSet<(int X, int Y)> Plants { get; set; } = [];
 	public int FenceCount { get; set; }
+	public int Sides { get; set; }
 }
